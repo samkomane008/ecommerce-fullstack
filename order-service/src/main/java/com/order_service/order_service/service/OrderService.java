d905897f390c9ad3fb6cf65099751bba22f311a2
Samuel Komane <sam.komane8@gmail.com>
2025-02-19 12:36:47 +0200

    add spring cloud open feign

--- order-service/src/main/java/com/order_service/order_service/service/OrderService.java
+++ order-service/src/main/java/com/order_service/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.order_service.order_service.service;
 
+import com.order_service.order_service.client.InventoryClient;
 import com.order_service.order_service.dto.OrderRequest;
 import com.order_service.order_service.model.Order;
 import com.order_service.order_service.repository.OrderRepository;
@@ -13,14 +14,21 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        orderRepository.save(order);
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        }
+
     }
 }
