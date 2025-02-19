76779b01b8da981626e8808dc357e61494752a1f
Samuel Komane <sam.komane8@gmail.com>
2025-02-19 12:11:45 +0200

    add inventory service - check is product is in stock

---
+++ inventory-service/src/main/java/com/inventory/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.inventory.repository;
+
+import com.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
+    boolean existsBySkuCodeAndQuantityGreaterThanEqual(String skuCode, Integer quantityIsGreaterThan);
+}
