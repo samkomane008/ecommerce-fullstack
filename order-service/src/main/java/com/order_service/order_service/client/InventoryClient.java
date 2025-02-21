96ec2d10820a42dec3c7aafbf254d24533cc0a0e
Samuel Komane <sam.komane8@gmail.com>
2025-02-21 10:13:59 +0200

    fixed order startup error -> cannot resolve url

--- order-service/src/main/java/com/order_service/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/order_service/order_service/client/InventoryClient.java
@@ -5,18 +5,15 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
 @Slf4j
-@FeignClient(value = "inventory", url = "http://localhost:8082") // to be removed
 public interface InventoryClient {
 
     Logger log = LoggerFactory.getLogger(InventoryClient.class);
 
-    @RequestMapping(method = RequestMethod.GET, value = "api/inventory") // to be changed to -> @GetExchange("/api/inventory")
+    @GetExchange("/api/inventory")
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
